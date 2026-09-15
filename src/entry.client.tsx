2b091c69b0fc3208d12e545c38974484dd6617fc
handshek <buneeisslo@gmail.com>
2026-09-15 20:56:20 +0530

    fix: tolerate document level browser extensions

--- src/entry.client.tsx
+++ src/entry.client.tsx
@@ -2,6 +2,13 @@ import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 import { HydratedRouter } from "react-router/dom";
 
+// React 19 tolerates extension markup inside head/body, but not beside them.
+for (const node of Array.from(document.documentElement.childNodes)) {
+  if (node !== document.head && node !== document.body) {
+    document.body.append(node);
+  }
+}
+
 startTransition(() => {
   hydrateRoot(
     document,
